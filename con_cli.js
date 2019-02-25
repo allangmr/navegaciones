0143a498dfc5965aa0bab0b5c820ec01656d17cc
rsamudio <rsamudiopinto@hotmail.com>
2019-02-25 17:11:19 -0500

    Cambio de controles y ordenes
    Cambio de controles y ordenes

--- con_cli.js
+++ con_cli.js
@@ -13,6 +13,7 @@ var $grid_aju;
 var $grid_busq;
 var $grid_deudatotal;
 var $grid_audi_mod;
+var $grid_cargo_fac;
 var $gridCorte;
 var opcion;
 var grid_historico_medidores;
@@ -24,7 +25,7 @@ var flag_med_prop = false;
 var flag_deuda_prop = false;
 var flag_audi_mod = false;
 var flag_hist_med_prop = false;
-
+var $grid_medidor_reg;
 $(document).ready(function() {
 
     // SE INICIALIZAN LOS GRIDS
@@ -945,7 +946,82 @@ function fn_setea_grilla_deuda_por_cargo(){
 	$grid_selector_deuda.pqGrid( "option", "scrollModel", {horizontal: true} );
 }
 
-
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*   
+function fn_setea_grilla_medidor_reg(){
+	
+    /**- Cuenta N&#176;: <%=Request("Suministro")%> **/
+        var obj3 = {
+            showTop: false,
+            width:'100%',
+            height:400,
+            rowBorders: true,
+            editable: false,
+            scrollModel:{theme:true},
+            colModel:
+            [
+                { title: "Medidor", width: 80, align: "center",dataIndx:"c1", editable: false},
+                { title: "Marca", width: 100, align: "left" ,dataIndx:"c2", editable: false},
+                { title: "Modelo", width: 120, align: "center" ,dataIndx:"c3", editable: false},
+                { title: "Diámetro", width: 150, align: "left" ,dataIndx:"c4", editable: false},
+                { title: "Medida", width: 100, align: "left" ,dataIndx:"c5", editable: false},
+                { title: "Factor", width: 120, align: "left" ,dataIndx:"c6", editable: false},
+                { title: "Enteros", width: 140, align: "left" ,dataIndx:"c7", editable: false},
+                { title: "Decimales", width: 80, align: "center" ,dataIndx:"c8", editable: false},
+                { title: "Instalación", width: 140, align: "left" ,dataIndx:"c9", editable: false},
+                { title: "Lec. Instala", width: 80, align: "center" ,dataIndx:"c10", editable: false},
+                { title: "Fec. Actualiza", width: 80, align: "center" ,dataIndx:"c11", editable: false},
+                { title: "Propiedad", width: 80, align: "center" ,dataIndx:"c12", editable: false}
+            ],
+            dataModel: {
+                paging: "local",
+                location: "local",
+                sorting: "local",
+                sortDir: "up"
+            },
+            collapsible: false,
+            selectionModel: { type: 'row',mode:'single'}
+        };
+        
+        $grid_medidor_reg =$("#grid_medidor_reg").pqGrid(obj3);
+        $grid_medidor_reg.pqGrid( "option", "scrollModel", {horizontal: true} );
+        $grid_medidor_reg.pqGrid("option", "pageModel.type", {checked:false});
+        $( "#grid_medidor_reg" ).pqGrid( "option", "showBottom", false );
+            
+        
+    }
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*   
+function fn_setea_grilla_cargo_fac(){
+	
+    /**- Cuenta N&#176;: <%=Request("Suministro")%> **/
+        var obj3 = {
+            showTop: false,
+            width:'100%',
+            height:400,
+            rowBorders: true,
+            editable: false,
+            scrollModel:{theme:true},
+            colModel:
+            [
+                { title: "Cargo Facturado", width: 400, align: "center",dataIndx:"c1", editable: false},
+                { title: "Valor Facturado", width: 400, align: "left" ,dataIndx:"c2", editable: false}
+            ],
+            dataModel: {
+                paging: "local",
+                location: "local",
+                sorting: "local",
+                sortDir: "up"
+            },
+            collapsible: false,
+            selectionModel: { type: 'row',mode:'single'}
+        };
+        
+        $grid_cargo_fac =$("#grid_cargos_fac").pqGrid(obj3);
+        $grid_cargo_fac.pqGrid( "option", "scrollModel", {horizontal: true} );
+        $grid_cargo_fac.pqGrid("option", "pageModel.type", {checked:false});
+        $( "#grid_cargos_fac" ).pqGrid( "option", "showBottom", false );
+            
+        
+    }
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*   
 function fn_setea_grilla_historico_medidores(){
 	
@@ -1068,7 +1144,8 @@ function fn_fac_cont()
     $("#div_deuda").hide();
     $("#div_abastece").hide();
     $("#div_historico_medidores").hide();
-
+    fn_setea_grilla_medidor_reg();
+    fn_setea_grilla_cargo_fac();
 }
 function fn_med_ver()	
 {	
@@ -1121,7 +1198,7 @@ function fn_historico_medidores_ver()
     $("#div_abastece").hide();
     $("#div_deuda").hide();
     $("#div_historico_medidores").show();
-    fn_setea_grilla_historico_medidores()
+    fn_setea_grilla_historico_medidores();
     $("#co_excel").html("<span class='glyphicon glyphicon-save'></span> Exportar Excel");
 }
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*        	
